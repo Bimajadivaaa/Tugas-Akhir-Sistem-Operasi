5e9df97d3b4d3e054cf2bc63d62a9c104fea0fb7
Oktavianus Bima Jadiva <93313454+Bimajadivaaa@users.noreply.github.com>
2022-12-13 12:04:48 +0700

    Update SO_TUGAS AKHIR.py

--- SO_TUGAS AKHIR.py
+++ SO_TUGAS AKHIR.py
@@ -23,10 +23,10 @@ class PrioritySchedullingNonPreemptive:
         for i in range (nomer_process):
             temp = []
             process_id = int(input("Masukkan ID Proccess : "))
-            
+            arrival_time = int(input(f"Masukkan Arrival Time untuk Process {process_id}: "))
             waktu_burst = int(input(f"Masukkan Burst Time untuk Proccess {process_id} : "))
             waktu_prioritas = int(input(f"Masukkan Prioritas untuk Proses {process_id} : "))
-            temp.extend([process_id, 0 , waktu_burst, waktu_prioritas])
+            temp.extend([process_id, 0 ,arrival_time, waktu_burst, waktu_prioritas])
             
             process_data.append(temp)
         return process_data
@@ -63,7 +63,7 @@ class PrioritySchedullingNonPreemptive:
         self.findWaitingTime(process_queue, waiting_time)
         self.findTurnAroundTime(process_queue, waiting_time, turnaround_time)
 
-        print("Process ID\tBurst Time\tWaiting Time\tTurnaround Time")
+        print("Process ID\tArrival Time\tWaiting Time\tTurnaround Time")
         total_waiting_time = 0
         total_turnaround_time = 0
         for i in range(len(process_queue)):
@@ -79,11 +79,12 @@ class PrioritySchedullingNonPreemptive:
         self.printGanttChart(process_queue)
         self.findavgTime(process_queue)
         self.printTable(process_queue)
-
+    
     def printTable(self, process_queue):
-        print("Process ID\tBurst Time\tPriority")
+        print("Process ID\tArrival Time\tBurst Time")
         for i in range(len(process_queue)):
-            print(str(process_queue[i].process_id) + "\t\t" + str(process_queue[i].burst_time) + "\t\t" + str(process_queue[i].priority))
+            print(str(process_queue[i].process_id) + "\t\t" + str(process_queue[i].burst_time) + 
+                  "\t\t" + str(process_queue[i].priority) + "\t\t")
 
             
     def run(self):
